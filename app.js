07c291bb1d7461eaba0c305d75abbe0dbc2c32ce
Prashantrajput11 <prashanttanwar461@gmail.com>
2021-08-15 13:56:50 +0530

    stocks app
    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var purchase_price = document.querySelector("#purchase-price");
+var quantity = document.querySelector("#quantity");
+var current_price = document.querySelector("#current-price");
+var show = document.querySelector("#show");
+var button = document.querySelector("button");
+
+button.addEventListener("click", checkProfitOrLoss);
+
+function checkProfitOrLoss() {
+  validateInputs();
+
+  if (current_price.value > purchase_price.value) {
+    var profit = (current_price.value - purchase_price.value) * quantity.value;
+    var profit_percentage = (profit / purchase_price.value) * 100;
+    console.log(typeof profit_percentage);
+    // profit
+    show.innerText = `you are at a profit of ${profit} and profit percentage is ${profit_percentage}%`;
+  } else if (current_price.value < purchase_price.value) {
+    //loss
+    var loss = (purchase_price.value - current_price.value) * quantity.value;
+    var loss_percentage = (loss / purchase_price.value) * 100;
+    show.innerText = `you are at a loss of ${loss} and loss percentage is ${loss_percentage}%`;
+    toggleTheme(loss_percentage);
+  } else {
+    // no change
+    console.log("no change");
+  }
+}
+
+function validateInputs() {
+  if (current_price.value === "" || purchase_price.value === "") {
+    show.innerText = `Fields can not be empty`;
+  }
+}
+
+function toggleTheme(loss_percentage) {
+  if (loss_percentage > 50) {
+    document.body.style.background = "red";
+  }
+}
